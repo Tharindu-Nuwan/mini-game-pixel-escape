0711664eb6ae959c217c988317be959c5e22dce0
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-12-31 10:50:54 +0530

    Implement Junp effect of the charactor

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 var boyElm = document.getElementById('boy-charactor');
 var bgImgElm = document.getElementById('bg-image');
-var idleImageNumber = 0;
-var runImageNumber = 0;
+var idleImageNumber = 1;
+var runImageNumber = 1;
+var jumpImageNumber = 1;
 var idleAnimationNumber = 0;
 var runAnimationNumber = 0;
+var jumpAnimationNumber = 0;
 var moveBgAnimId = 0;
 
 function idleAnimation() {
@@ -42,16 +44,59 @@ function keyCheck(event) {
         if (runAnimationNumber == 0) {
             runAnimationStart();
         }
+        if(moveBgAnimId == 0) {
+            moveBgAnimId = setInterval(moveBackground, 40);
+        }
     }
 
-    if(moveBgAnimId == 0) {
-        moveBgAnimId = setInterval(moveBackground, 30);
+    if (keyCode == 32) {
+        if (jumpAnimationNumber == 0) {
+            jumpAnimationStart();
+        }
+        if(moveBgAnimId == 0) {
+            moveBgAnimId = setInterval(moveBackground, 23);
+        }
     }
 }
 
 var backgroundPosition = 0;
 
 function moveBackground() {
-    backgroundPosition = backgroundPosition - 10
+    backgroundPosition = backgroundPosition - 10;
     bgImgElm.style.backgroundPositionX = backgroundPosition + "px"
 }
+
+var boyMarginTop = 345;
+
+function jumpAnimation() {
+    jumpImageNumber = jumpImageNumber + 1;
+
+    if (jumpImageNumber <= 7) {
+        boyMarginTop = boyMarginTop - 20;
+        boyElm.style.marginTop = boyMarginTop + "px";
+    }
+
+    if (jumpImageNumber >= 8) {
+        boyMarginTop = boyMarginTop + 20;
+        boyElm.style.marginTop = boyMarginTop + "px";
+    }
+
+    if (jumpImageNumber == 13) {
+        jumpImageNumber = 1;
+        clearInterval(jumpAnimationNumber);
+        jumpAnimationNumber = 0;
+        runImageNumber = 1;
+        runAnimationStart();
+    }
+
+    boyElm.src = "resources/charactor/Jump ("+ jumpImageNumber +").png";
+
+}
+
+function jumpAnimationStart() {
+    clearInterval(idleAnimationNumber);
+    runImageNumber = 0;
+    clearInterval(runAnimationNumber);
+    jumpAnimationNumber = setInterval(jumpAnimation, 100);
+}
+
