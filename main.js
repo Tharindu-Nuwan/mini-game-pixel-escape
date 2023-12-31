67d7f4b5a1652d6df9f0e48c335346ede565139c
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-12-31 11:59:56 +0530

    Create barriers running towards charactor

--- main.js
+++ main.js
@@ -1,102 +1,138 @@
-var boyElm = document.getElementById('boy-charactor');
-var bgImgElm = document.getElementById('bg-image');
+var boyElm = document.getElementById("boy-charactor");
+var bgImgElm = document.getElementById("bg-image");
 var idleImageNumber = 1;
 var runImageNumber = 1;
 var jumpImageNumber = 1;
 var idleAnimationNumber = 0;
 var runAnimationNumber = 0;
 var jumpAnimationNumber = 0;
+var barrierAnimationId = 0;
+
 var moveBgAnimId = 0;
 
 function idleAnimation() {
-    idleImageNumber = idleImageNumber + 1;
+  idleImageNumber = idleImageNumber + 1;
 
-    if (idleImageNumber == 11) {
-        idleImageNumber = 1;
-    }
+  if (idleImageNumber == 11) {
+    idleImageNumber = 1;
+  }
 
-    boyElm.src = "resources/charactor/Idle ("+ idleImageNumber +").png";
+  boyElm.src = "resources/charactor/Idle (" + idleImageNumber + ").png";
 }
 
 function idleAnimationStart() {
-    idleAnimationNumber = setInterval(idleAnimation, 100);
+  idleAnimationNumber = setInterval(idleAnimation, 100);
 }
 
 function runAnimation() {
-    runImageNumber = runImageNumber + 1;
+  runImageNumber = runImageNumber + 1;
 
-    if (runImageNumber == 9) {
-        runImageNumber = 1;
-    }
+  if (runImageNumber == 9) {
+    runImageNumber = 1;
+  }
 
-    boyElm.src = "resources/charactor/Run ("+ runImageNumber +").png";
+  boyElm.src = "resources/charactor/Run (" + runImageNumber + ").png";
 }
 
 function runAnimationStart() {
-    runAnimationNumber = setInterval(runAnimation, 50);
-    clearInterval(idleAnimationNumber);
-}
-
-function keyCheck(event) {
-    var keyCode = event.which;
-
-    if (keyCode == 13) {
-        if (runAnimationNumber == 0) {
-            runAnimationStart();
-        }
-        if(moveBgAnimId == 0) {
-            moveBgAnimId = setInterval(moveBackground, 40);
-        }
-    }
-
-    if (keyCode == 32) {
-        if (jumpAnimationNumber == 0) {
-            jumpAnimationStart();
-        }
-        if(moveBgAnimId == 0) {
-            moveBgAnimId = setInterval(moveBackground, 23);
-        }
-    }
+  runAnimationNumber = setInterval(runAnimation, 50);
+  clearInterval(idleAnimationNumber);
 }
 
 var backgroundPosition = 0;
 
 function moveBackground() {
-    backgroundPosition = backgroundPosition - 10;
-    bgImgElm.style.backgroundPositionX = backgroundPosition + "px"
+  backgroundPosition = backgroundPosition - 10;
+  bgImgElm.style.backgroundPositionX = backgroundPosition + "px";
 }
 
-var boyMarginTop = 345;
+var boyMarginTop = 330;
 
 function jumpAnimation() {
-    jumpImageNumber = jumpImageNumber + 1;
+  jumpImageNumber = jumpImageNumber + 1;
 
-    if (jumpImageNumber <= 7) {
-        boyMarginTop = boyMarginTop - 20;
-        boyElm.style.marginTop = boyMarginTop + "px";
-    }
+  if (jumpImageNumber <= 7) {
+    boyMarginTop = boyMarginTop - 20;
+    boyElm.style.marginTop = boyMarginTop + "px";
+  }
 
-    if (jumpImageNumber >= 8) {
-        boyMarginTop = boyMarginTop + 20;
-        boyElm.style.marginTop = boyMarginTop + "px";
-    }
+  if (jumpImageNumber >= 8) {
+    boyMarginTop = boyMarginTop + 20;
+    boyElm.style.marginTop = boyMarginTop + "px";
+  }
 
-    if (jumpImageNumber == 13) {
-        jumpImageNumber = 1;
-        clearInterval(jumpAnimationNumber);
-        jumpAnimationNumber = 0;
-        runImageNumber = 1;
-        runAnimationStart();
-    }
+  if (jumpImageNumber == 13) {
+    jumpImageNumber = 1;
+    clearInterval(jumpAnimationNumber);
+    jumpAnimationNumber = 0;
+    runImageNumber = 1;
+    runAnimationStart();
+  }
 
-    boyElm.src = "resources/charactor/Jump ("+ jumpImageNumber +").png";
-
+  boyElm.src = "resources/charactor/Jump (" + jumpImageNumber + ").png";
 }
 
 function jumpAnimationStart() {
-    clearInterval(idleAnimationNumber);
-    runImageNumber = 0;
-    clearInterval(runAnimationNumber);
-    jumpAnimationNumber = setInterval(jumpAnimation, 100);
+  clearInterval(idleAnimationNumber);
+  runImageNumber = 0;
+  clearInterval(runAnimationNumber);
+  jumpAnimationNumber = setInterval(jumpAnimation, 100);
 }
 
+barrierMarginLeft = 2000;
+
+function createBariers() {
+  for (let i = 0; i < 20; i++) {
+    var barrierElm = document.createElement("div");
+    barrierElm.className = "barrier";
+    bgImgElm.appendChild(barrierElm);
+    barrierElm.style.marginLeft = barrierMarginLeft + "px";
+    barrierElm.id = "barrier" + i;
+
+    if (i < 10) {
+        barrierMarginLeft = barrierMarginLeft + 1000; 
+    }
+
+    if (i >= 11) {
+        barrierMarginLeft = barrierMarginLeft + 500; 
+    }
+  }
+}
+
+function barrierAnimation() {
+    for (let i = 0; i < 20; i++) {
+        var barrier = document.getElementById("barrier" + i);
+        var currentMarginLeft = getComputedStyle(barrier).marginLeft;
+        var newMarginLeft = parseInt(currentMarginLeft) - 15;
+        barrier.style.marginLeft = newMarginLeft + "px";
+    }
+  }
+
+
+function keyCheck(event) {
+    var keyCode = event.which;
+  
+    if (keyCode == 13) {
+      if (runAnimationNumber == 0) {
+        runAnimationStart();
+      }
+      if (moveBgAnimId == 0) {
+        moveBgAnimId = setInterval(moveBackground, 40);
+      }
+      if (barrierAnimationId == 0) {
+          barrierAnimationId = setInterval(barrierAnimation, 40);
+      }
+    }
+  
+    if (keyCode == 32) {
+      if (jumpAnimationNumber == 0) {
+        jumpAnimationStart();
+      }
+      if (moveBgAnimId == 0) {
+        moveBgAnimId = setInterval(moveBackground, 23);
+      }
+      if (barrierAnimationId == 0) {
+          barrierAnimationId = setInterval(barrierAnimation, 100);
+      }
+    }
+  }
